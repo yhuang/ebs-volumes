b83ba077474df80bafd266ff05444e6664e03554
Jimmy Huang <jimmy.huang@duragility.com>
2023-12-27 23:06:36 -0800

    iterating to additional ebs volumes

--- ebs-volumes.tf
+++ ebs-volumes.tf
@@ -1,6 +1,6 @@
 resource "aws_ebs_volume" "acceptance" {
   for_each = local.acceptance_instance_set
 
-  availability_zone = each.availability_zone
+  availability_zone = each.value.availability_zone
   size              = 16
 }
