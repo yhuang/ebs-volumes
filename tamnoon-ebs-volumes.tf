325fb7e84f3a3951719211f792c32ad13708747e
Jimmy Huang <jimmy.huang@duragility.com>
2023-12-29 02:22:59 -0800

    fixed ebs volume definition

--- tamnoon-ebs-volumes.tf
+++ tamnoon-ebs-volumes.tf
@@ -19,7 +19,7 @@ resource "aws_ebs_volume" "tamnoon_private" {
 resource "aws_ebs_volume" "tamnoon_multi_attach" {
   availability_zone = local.instance_input["availability-zone"]
   size              = 16
-  iops              = 32000
+  iops              = 8
   type              = "io1"
 
   multi_attach_enabled = true
