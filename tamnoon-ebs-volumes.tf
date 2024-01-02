0222111944d4ea24febe3d81f110a4944665270f
Jimmy Huang <jimmy.huang@duragility.com>
2024-01-02 14:11:45 -0800

    io1 iops 100 => 3000

--- tamnoon-ebs-volumes.tf
+++ tamnoon-ebs-volumes.tf
@@ -35,7 +35,7 @@ resource "aws_ebs_volume" "tamnoon_private" {
 resource "aws_ebs_volume" "tamnoon_multi_attach" {
   availability_zone = local.instance_input["availability-zone"]
   size              = 16
-  iops              = 100
+  iops              = 3000
   type              = "io1"
 
   multi_attach_enabled = true
